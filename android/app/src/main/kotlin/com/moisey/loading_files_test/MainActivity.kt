ca6578c9e08a0b4873861070d077b9c2ec186c43
moisey <moisey312@gmail.com>
2020-07-09 16:32:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/moisey/loading_files_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moisey.loading_files_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/filesScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FilesScreen extends StatefulWidget {
+  @override
+  _FilesScreenState createState() => _FilesScreenState();
+}
+
+class _FilesScreenState extends State<FilesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
