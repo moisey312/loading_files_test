74219e8fb5abb817048d8c261584eb6b28c38ad0
moisey <moisey312@gmail.com>
2020-07-10 12:40:35 +0300

    class of file and it's functions

--- lib/file.dart
+++ lib/file.dart
@@ -1,3 +1,32 @@
-class File{
-  
+import 'dart:math';
+
+import 'constants.dart';
+
+enum fileStatus { waiting, uploading, uploaded }
+
+class File {
+  String fileName;
+  fileStatus status;
+  int id;
+  int time;
+
+  File(this.id, this.status, this.time) {
+    fileName = 'Файл ' + id.toString();
+    queueOfFiles.add(id);
+  }
+}
+
+addFile() {
+  Random rnd = Random();
+  int time = rnd.nextInt(5) + 1;
+  files.add(File(
+      counter,
+      queueOfFiles.length >= 3 ? fileStatus.waiting : fileStatus.uploaded,
+      time));
+  counter += 1;
+  print(files.length);
+}
+
+removeFile(File file) {
+  files.remove(file);
 }
